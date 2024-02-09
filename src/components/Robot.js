8d1e50371a14de39568ec665657ece2a89edd4b9
Ibrahim1IA <issoufouibrahimm2@gmail.com>
2024-02-09 20:04:30 +0100

    first commit

---
+++ src/components/Robot.js
@@ -0,0 +1,69 @@
+export const robots = [
+    {
+        id : 1,
+        name : 'Mohamed issoufou',
+        username : 'ABdoulaye',
+        emaail : 'moha@gmail.com'
+    },
+    {
+        id : 2,
+        name : 'Ibrahim issoufou',
+        username : 'ABdoulaye',
+        emaail : 'moha@gmail.com'
+    },
+    {
+        id : 3,
+        name : 'Najim issoufou',
+        username : 'ABdoulaye',
+        emaail : 'moha@gmail.com'
+    },
+
+    {
+        id : 4,
+        name : 'Mohamed issoufou',
+        username : 'ABdoulaye',
+        emaail : 'moha@gmail.com'
+    },
+
+    {
+        id : 5,
+        name : 'Mohamed issoufou',
+        username : 'ABdoulaye',
+        emaail : 'moha@gmail.com'
+    },
+
+    {
+        id : 6,
+        name : 'Mohamed issoufou',
+        username : 'ABdoulaye',
+        emaail : 'moha@gmail.com'
+    },
+
+    {
+        id : 7,
+        name : 'Mohamed issoufou',
+        username : 'ABdoulaye',
+        emaail : 'moha@gmail.com'
+    },
+
+    {
+        id : 8,
+        name : 'Mohamed issoufou',
+        username : 'ABdoulaye',
+        emaail : 'moha@gmail.com'
+    },
+
+    {
+        id : 9,
+        name : 'Mohamed issoufou',
+        username : 'ABdoulaye',
+        emaail : 'moha@gmail.com'
+    },
+
+    {
+        id : 10,
+        name : 'Mohamed issoufou',
+        username : 'ABdoulaye',
+        emaail : 'moha@gmail.com'
+    },
+];
